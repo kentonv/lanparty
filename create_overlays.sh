87801e0ffbffdf18586cb300beb6ea12ad3ab62b
Kenton Varda <kenton@cloudflare.com>
2019-12-19 14:18:57 -0800

    Factor out a bunch of configuration variables.

--- create_overlays.sh
+++ create_overlays.sh
@@ -4,6 +4,14 @@ set -euo pipefail
 
 MACHINES="cutman gutsman iceman bombman fireman elecman metalman airman bubbleman quickman crashman flashman"
 EXTENTS=39744
+VGROUP=bigdisks
+BASE_IMAGE=gamestation-win7
+EXPORT_DEVS=/dev/gamestations
+OVERLAY_DEVICE=/dev/sdb
+CACHE_LOOP_DEVICE=/dev/loop7
+# Use /dev/loop7 to avoid interfering with any loop devices LVM may have auto-created.
+UPDATES_MACHINE=flashman
+LOCAL_MOUNT_POINT=/mnt/gamestation
 
 DRY_RUN=no
 DELETE=no
@@ -11,7 +19,13 @@ MERGE=no
 ONLYONE=no
 ONLYTWO=no
 PAGECACHE=yes
-BASE_IMAGE=/dev/bigdisks/gamestation-win7
+
+# TODO:
+# - Revamp CLI
+# - Allow dynamic specification of update machine
+# - Remove -win7 suffix
+# - Remove devstation and win10 stuff
+# - Remove onlytwo
 
 while [ $# -gt 0 ]; do
   case "$1" in
@@ -24,11 +38,11 @@ while [ $# -gt 0 ]; do
       read
       ;;
     -l )
-      BASE_IMAGE=/dev/bigdisks/devstation-ubuntu
+      BASE_IMAGE=devstation-ubuntu
       echo "Configuring for Linux..."
       ;;
     --win10 )
-      BASE_IMAGE=/dev/bigdisks/gamestation-win10
+      BASE_IMAGE=gamestation-win10
       echo "Configuring for Windows 10..."
       ;;
     -m )
@@ -61,7 +75,7 @@ function doit {
 }
 
 if [ $ONLYONE == yes ]; then
-  MACHINES=flashman
+  MACHINES=$UPDATES_MACHINE
   EXTENTS=476932
   PAGECACHE=no
 elif [ $ONLYTWO == yes ]; then
@@ -84,8 +98,8 @@ if [ $MERGE == yes -o $DELETE == yes ]; then
   doit sleep 2
 #  doit service iscsitarget stop
 #  doit sleep 2
-  if (mount | grep -q /mnt/gamestation); then
-    doit umount /mnt/gamestation
+  if (mount | grep -q $LOCAL_MOUNT_POINT); then
+    doit umount $LOCAL_MOUNT_POINT
   fi
   if (mount | grep -q /mnt/devstation); then
     doit umount /mnt/devstation
@@ -94,63 +108,62 @@ else
   # Bringing up.
 
   # Remove old loopback mapping if it exists.
-  if (losetup | grep -q '^/dev/loop7'); then
-    doit losetup -d /dev/loop7
+  if (losetup | grep -q "^$CACHE_LOOP_DEVICE"); then
+    doit losetup -d $CACHE_LOOP_DEVICE
   fi
 
   if [ $PAGECACHE == yes ]; then
     # Setup loopback device on top of master image in order to get caching.
-    # Use /dev/loop7 to avoid interfering with any loop devices LVM may have auto-created.
-    doit losetup-new --direct-io=off --read-only /dev/loop7 /dev/bigdisks/gamestation-win7
+    doit losetup-new --direct-io=off --read-only $CACHE_LOOP_DEVICE /dev/$VGROUP/$BASE_IMAGE
   fi
 fi
 
-MASTER_SIZE=$(blockdev --getsz /dev/bigdisks/gamestation-win7)
+MASTER_SIZE=$(blockdev --getsz /dev/$VGROUP/$BASE_IMAGE)
 
-doit rm -rf /dev/gamestations
-doit mkdir -p /dev/gamestations
+doit rm -rf $EXPORT_DEVS
+doit mkdir -p $EXPORT_DEVS/internal
 
 for MACHINE in $MACHINES; do
   echo "================ $MACHINE ================"
 
   if [ $MERGE == yes -o $DELETE == yes ]; then
     # When deleting, delete everything.
-    # When merging, delete everything except flashman.
-    if [ $DELETE == yes -o $MACHINE != flashman ]; then
-      if [ -e /dev/mapper/cached-$MACHINE ]; then
-        doit dmsetup remove /dev/mapper/cached-$MACHINE
+    # When merging, delete everything except $UPDATES_MACHINE.
+    if [ $DELETE == yes -o $MACHINE != $UPDATES_MACHINE ]; then
+      if [ -e $EXPORT_DEVS/internal/cached-$MACHINE ]; then
+        doit dmsetup remove $EXPORT_DEVS/internal/cached-$MACHINE
       fi
-      if [ -e /dev/bigdisks/$MACHINE-win7 ]; then
-        doit lvremove -f /dev/bigdisks/$MACHINE-win7
+      if [ -e /dev/$VGROUP/$MACHINE-win7 ]; then
+        doit lvremove -f /dev/$VGROUP/$MACHINE-win7
       fi
-      if [ -e /dev/bigdisks/$MACHINE-cow ]; then
-        doit lvremove -f /dev/bigdisks/$MACHINE-cow
+      if [ -e /dev/$VGROUP/$MACHINE-cow ]; then
+        doit lvremove -f /dev/$VGROUP/$MACHINE-cow
       fi
     fi
   elif [ $PAGECACHE == yes ]; then
     # Create a regular volume with LVM.
-    doit lvcreate -n $MACHINE-cow -l $EXTENTS bigdisks /dev/sdb
+    doit lvcreate -n $MACHINE-cow -l $EXTENTS $VGROUP $OVERLAY_DEVICE
 
     # Use it as a raw devicemapper COW device.
-    doit dmsetup create cached-$MACHINE --table "0 $MASTER_SIZE snapshot /dev/loop7 /dev/bigdisks/$MACHINE-cow N 128"
+    doit dmsetup create cached-$MACHINE --table "0 $MASTER_SIZE snapshot $CACHE_LOOP_DEVICE /dev/$VGROUP/$MACHINE-cow N 128"
 
-    doit ln -s /dev/mapper/cached-$MACHINE /dev/gamestations/$MACHINE
+    doit ln -s $EXPORT_DEVS/internal/cached-$MACHINE $EXPORT_DEVS/$MACHINE
   else
-    doit lvcreate -c 64k -n $MACHINE-win7 -l $EXTENTS -s $BASE_IMAGE /dev/sdb
-    doit ln -s /dev/bigdisks/$MACHINE-win7 /dev/gamestations/$MACHINE
+    doit lvcreate -c 64k -n $MACHINE-win7 -l $EXTENTS -s /dev/$VGROUP/$BASE_IMAGE $OVERLAY_DEVICE
+    doit ln -s /dev/$VGROUP/$MACHINE-win7 $EXPORT_DEVS/$MACHINE
   fi
 done
 
 if [ $MERGE == yes ]; then
-  doit lvconvert --merge /dev/bigdisks/flashman-win7
+  doit lvconvert --merge /dev/$VGROUP/$UPDATES_MACHINE-win7
 fi
 
 if [ $MERGE != yes -a $DELETE != yes ]; then
   echo "================ start iscsi ================"
 
-#  doit mount -o ro,offset=1048576 /dev/bigdisks/gamestation-win7 /mnt/gamestation/
+#  doit mount -o ro,offset=1048576 /dev/$VGROUP/$BASE_IMAGE $LOCAL_MOUNT_POINT/
 #  TODO: What's the right offset? Is there a partition table?
-#  doit mount -o ro,offset=1048576 /dev/bigdisks/devstation-win7 /mnt/devstation/
+#  doit mount -o ro,offset=1048576 /dev/$VGROUP/devstation-win7 /mnt/devstation/
 #  doit service iscsitarget start
 #  doit systemctl start tgt
 
@@ -165,7 +178,7 @@ if [ $MERGE != yes -a $DELETE != yes ]; then
   TID=1
   for MACHINE in $MACHINES; do
     doit tgtadm -C 0 --lld iscsi --op new --mode target --tid $TID -T iqn.2001-04.com.kentonshouse.protoman:$MACHINE-win7
-    doit tgtadm -C 0 --lld iscsi --op new --mode logicalunit --tid $TID --lun 1 -b /dev/gamestations/$MACHINE
+    doit tgtadm -C 0 --lld iscsi --op new --mode logicalunit --tid $TID --lun 1 -b $EXPORT_DEVS/$MACHINE
     doit tgtadm -C 0 --lld iscsi --op bind --mode target --tid $TID -I ALL
     TID=$(( TID + 1 ))
   done
